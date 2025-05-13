39234a1438f70ec0fe3f8175aaef0f87ea5eebe2
Sroopesh1122 <shivuroopesh6362@gmail.com>
2025-05-13 10:42:44 +0530

    first commit

---
+++ src/main/java/com/pentagon/app/PengtagonApplication.java
@@ -0,0 +1,13 @@
+package com.pentagon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengtagonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengtagonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pentagon/app/PengtagonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentagon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengtagonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
