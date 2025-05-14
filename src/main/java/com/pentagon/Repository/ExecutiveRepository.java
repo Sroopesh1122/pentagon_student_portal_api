f2eb969ab2c95776de402e306258b1382d18c22a
Sahanagowda30 <sahana.learn@gmail.com>
2025-05-14 10:53:22 +0530

    Entity,Repo,Service created

---
+++ src/main/java/com/pentagon/Repository/ExecutiveRepository.java
@@ -0,0 +1,9 @@
+package com.pentagon.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pentagon.Entity.Executive;
+
+public interface ExecutiveRepository extends JpaRepository<Integer, Executive>{
+
+}
