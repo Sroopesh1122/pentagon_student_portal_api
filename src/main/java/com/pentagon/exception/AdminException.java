0b13c241a50d50275c98d31ef3047bde8f39a62a
pradeepkumar-s-2001 <pradeepkurba2001@gmail.com>
2025-05-15 10:48:05 +0530

    Exception and response is completed

---
+++ src/main/java/com/pentagon/exception/AdminException.java
@@ -0,0 +1,22 @@
+package com.pentagon.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class AdminException extends RuntimeException {
+
+		private static final long serialVersionUID = 1L;
+
+		private HttpStatus httpStatus;
+
+		public AdminException(String message , HttpStatus httpStatus) {
+			super(message);
+			this.httpStatus = httpStatus;
+			this.httpStatus = httpStatus!=null ? httpStatus : HttpStatus.INTERNAL_SERVER_ERROR;	
+		}
+
+		public HttpStatus getHttpStatus() {
+			return httpStatus;
+		}
+	}
+
+
