2718f5960aa0f638f864eb134e280a51ad5649c7
pradeepkumar-s-2001 <pradeepkurba2001@gmail.com>
2025-05-15 12:36:36 +0530

    trainer exception

---
+++ src/main/java/com/pentagon/exception/TrainerException.java
@@ -0,0 +1,19 @@
+package com.pentagon.exception;
+
+
+
+import org.springframework.http.HttpStatus;
+
+public class TrainerException extends RuntimeException {
+    private HttpStatus httpStatus;
+    private static final long serialVersionUID = 1L;
+
+    public TrainerException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
